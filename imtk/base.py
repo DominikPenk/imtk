ecdea79097054050c5985abac9e51ae0181231b2
Dominik Penk <penk.dominik@gmail.com>
2023-11-03 11:16:08 +0100

    Started to add some docstrings

--- imtk/base.py
+++ imtk/base.py
@@ -7,7 +7,8 @@ from .cursor import ImCursor
 
 
 class ImWidgetState(object):
-    """Represents the state of an Immediate Mode GUI widget.
+    """
+    Represents the state of an Immediate Mode GUI widget.
 
     This class stores information about a widget's state, including its widget object,
     position, custom data, and identifier.
@@ -16,7 +17,7 @@ class ImWidgetState(object):
         widget (tk.Widget): The tkinter widget associated with this state.
         identifier (str): A unique identifier for the widget.
         position (Optional[Tuple[int, int]]): The position of the widget (x, y).
-        new (bool): Indicates if the widget is new.
+        new (bool): Indicates if the widget is newly created.
         drawn (bool): Indicates if the widget has been drawn.
         custom_data (Optional[Dict[str, Any]]): Custom data associated with the widget.
     """
@@ -38,12 +39,12 @@ class ImWidgetState(object):
 
 
 class ImNamespace:
-    """Represents a namespace within an Immediate Mode GUI frame.
+    """Represents a namespace within an Immediate Mode GUI context.
 
     Namespaces are an easy way to avoid identifier conflicts.
 
     Args:
-        context (ImFrame): The parent frame to which this namespace belongs.
+        context (ImContext): The parent frame to which this namespace belongs.
         name (str): The name of the namespace.
     """
     def __init__(self, context:'ImContext', name:str):
@@ -60,6 +61,13 @@ class ImNamespace:
 
 
 class ImContext(abc.ABC):
+    """
+    Represents the main context for the Immediate Mode GUI.
+
+    Args:
+        refresh_mode (str): The mode for refreshing the GUI ('callback' or 'loop').
+        loop_frequency (int): Frequency of the loop when using the 'loop' refresh mode.
+    """
     __active__: ImContext | None = None
 
     def __init__(
@@ -193,14 +201,54 @@ class ImContext(abc.ABC):
             self.yview('moveto', str(first))
 
 
-    def set_refresh_engine(self, method:str, do_refresh:bool=True):
+    def set_refresh_engine(self, method:str|int, loop_frequency:int|None=None):
+        """
+        Set the refresh engine mode and, if applicable, the loop frequency.
+
+        This method allows you to configure the refresh engine mode for the context
+        When using 'loop' mode, you may specify the frequency at which the GUI content will be refreshed.
+
+        Args:
+            method (str | int): The refresh mode to be used. Choose from:
+                - 'callback': GUI content is refreshed on-demand when specific events trigger
+                updates. Ideal for event-driven GUIs where updates occur in response to user actions.
+                - 'loop': GUI content is refreshed at a specified frequency, creating a continuous
+                and real-time rendering loop. Suitable for applications where the content changes
+                without user interaction.
+                - An integer: This is equivalent to passing 'loop' and a loop_frequency
+
+            loop_frequency (int | None): The loop frequency, applicable only when 'loop' mode is selected.
+                It represents the time interval (in milliseconds) between each refresh cycle. If method is
+                'loop', loop_frequency is required. If method is an integer.
+
+        Returns:
+            None
+
+        Example:
+            To set the refresh engine to 'callback' mode and initiate refreshing:
+            >>> set_refresh_engine('callback')
+
+            To set the refresh engine to 'loop' mode with a refresh frequency of 60 milliseconds:
+            >>> set_refresh_engine('loop', loop_frequency=60)
+
+            To change the loop frequency without altering the refresh mode:
+            >>> set_refresh_engine(30)  # Sets the loop frequency to 30 milliseconds
+        """
+        if isinstance(method, int):
+            if loop_frequency is not None:
+                raise ValueError("if method is an integer, loop_frequency must be None")
+            self.loop_frequency = method
+            self.method = 'loop'
+
         if method not in ['callback', 'loop']:
             raise ValueError("method must be 'callback' or 'loop'")
         self._refresh_mode = method
         if self._after_id:
             self.after_cancel(self._after_id)
-        if do_refresh:
-            self.refresh()
+        
+        if method == 'loop' and loop_frequency is not None:
+            self.loop_frequency = loop_frequency 
+        self.refresh()
 
 
     def init_cursor(self) -> ImCursor:
@@ -208,6 +256,14 @@ class ImContext(abc.ABC):
 
 
     def yview(self, *args):
+        """
+        Scroll the GUI content vertically.
+
+        This method is responsible for vertical scrolling of the Immediate Mode GUI content.
+        It adjusts the scrollbar position and the content frame's relative placement to
+        achieve the desired scrolling effect. The method is typically invoked in response to
+        user interactions with the scrollbar or mouse wheel.
+        """
         if args[0] == "moveto":
             base, thumb = self._get_scroll_measure()
             fraction = float(args[1])
@@ -222,6 +278,20 @@ class ImContext(abc.ABC):
 
 
     def refresh(self) -> None:
+        """
+        Refresh the Immediate Mode GUI content.
+
+        This method is the core of the ImTk, ensuring that the GUI content is up to date, 
+        correctly drawn, and responsive to user interactions. 
+        It also manages the refresh mode and cursor stack to maintain GUI consistency.
+
+        Note:
+        - When using the 'loop' refresh mode, the refresh cycle is scheduled at a specified
+        frequency to provide real-time updates and interactivity.
+
+        - This method should not be called directly by users; it is automatically called by
+        the framework to manage GUI rendering and updates.
+        """
         # Prevent interlaced refesh calles due to parallelism
         # Maybe use a lock instead?
         if ImContext.__active__:
@@ -292,6 +362,20 @@ class ImContext(abc.ABC):
 
 
     def get_identifier(self, val:str):
+        """
+        Get a namespaced identifier for a value within the current context.
+
+        Args:
+            val (str): The value to be namespaced.
+
+        Returns:
+            str: The namespaced identifier.
+
+        Note:
+            The format of the namespaced identifier includes the current namespace's name,
+            followed by a double colon (::), and the provided value. This format helps
+            ensure unique and context-specific identifiers.
+        """
         return f"{self.current_namespace}::{val}" if self.current_namespace else val
 
 
@@ -310,6 +394,12 @@ class ImContext(abc.ABC):
 
 
     def get_current_content_frame(self):
+        """
+        Get the current content frame for GUI drawing.
+
+        Returns:
+            tk.Widget: The content frame for drawing.
+        """
         return self.cursor.get_frame_widget() or self._content_frame
 
 
@@ -320,6 +410,18 @@ class ImContext(abc.ABC):
         master:tk.Widget=None, 
         **kwargs
     ) -> tk.Widget:
+        """
+        Create a new GUI widget of the specified type. You probably will never call this
+        method yourself it is internally called by the immediate GUI functions.
+
+        Args:
+            type (str): The type of the widget to create.
+            master (tk.Widget): The parent widget to contain the new widget. (Default is None.)
+            **kwargs: Additional keyword arguments for widget configuration.
+
+        Returns:
+            tk.Widget: The newly created widget.
+        """
         if type not in self._widget_factories:
             msg = f"Widget '{type}' was not installed with your backend implementation" \
                    "check out the 'install_widgets' method."
@@ -330,6 +432,11 @@ class ImContext(abc.ABC):
 
     @abc.abstractmethod
     def draw(self) -> None:
+        """
+        Abstract method for drawing the GUI content.
+
+        This method must be implemented in subclasses to define the GUI layout and elements.
+        """
         pass
     
 
@@ -343,27 +450,79 @@ class ImContext(abc.ABC):
 
     @property
     def current_namespace(self) -> str:
+        """
+        Get the current namespace.
+
+        Returns:
+            str: The name of the current namespace.
+        """
         return self._namespaces[-1] if self._namespaces else ""
 
 
     @property
     def cursor(self) -> ImCursor:
+        """
+        Get the current cursor for managing GUI drawing.
+
+        Returns:
+            ImCursor: The current cursor.
+        """
         if not self._cursor_stack:
             raise RuntimeError("No active cursor found")
         return self._cursor_stack[-1]
 
 
 def get_context() -> ImContext:
+    """
+    Get the currently active Immediate Mode GUI context.
+
+    Returns:
+        ImContext: The active GUI context.
+    """
     if ImContext.__active__ is None:
         raise RuntimeError("No active context")
     return ImContext.__active__
 
 
 def namespace(name:str, context:Optional[ImContext]=None) -> ImNamespace:
+    """
+    Create namespace within an Immediate Mode GUI context.
+
+    Namespaces are a mechanism for organizing and isolating identifiers and widgets within an
+    Immediate Mode GUI context. They help prevent naming conflicts.
+
+    Args:
+        name (str): The name of the namespace to create..
+
+        context (ImContext | None): The Immediate Mode GUI context in which the namespace should be
+            created. If provided, the namespace is associated with the specified context. If not
+            provided, the function uses the active context. (Default is None.)
+
+    Returns:
+        ImNamespace: An ImNamespace instance representing the created namespace.
+
+    Example:
+        # Create a namespace within the active context using a 'with' statement
+        with namespace("my_namespace"):
+            # Within this block, all widget identifiers and actions are scoped to 'my_namespace'
+            if imtk.button("A button"):
+                print("The button was clicked")
+    """
     context = context or get_context()
     return context.namespace(name)
 
 
 def get_identifier(val:str, context:Optional[ImContext]=None) -> str:
+    """
+    Get a namespaced identifier for a value within a specific context.
+
+    Args:
+        val (str): The value to be namespaced.
+        context (Optional[ImContext]): The context in which to get the identifier.
+            If not provided, the function uses the currently active context, if available.
+
+    Returns:
+        str: The namespaced identifier.
+    """
     context = context or get_context()
     return context.get_identifier(val)
--- imtk/frames.py
+++ imtk/frames.py
@@ -12,6 +12,27 @@ def frame(
     context: base.ImContext | None = None,
     **kwargs
 ) -> ImCursor:
+    """
+    Create a frame widget within the Immediate Mode GUI context and return a cursor for drawing in it.
+
+    This function creates a frame widget, which is a container for grouping other widgets within
+    the GUI.
+
+    Args:
+        identifier (str): A unique identifier for the frame widget.
+        context (ImContext | None): The Immediate Mode GUI context in which the frame should be created.
+            If not provided, the active context is used. (Default is None.)
+        **kwargs: Additional keyword arguments to configure the frame widget.
+
+    Returns:
+        ImCursor: A cursor object for configuring and adding child widgets to the frame.
+
+    Example:
+        # Create a frame widget with the identifier "my_frame"
+        with frame("my_frame"):
+            # Within this block, all widget identifiers and actions are scoped to "my_frame"
+            ...
+    """
     # Create the widget and add it
     context = context or base.get_context()
 
@@ -36,6 +57,27 @@ def labelframe(
     context: base.ImContext | None = None,
     **kwargs
 ) -> ImCursor:
+    """
+    Create a labeled frame widget within the Immediate Mode GUI and return a cursor for configuring it.
+
+    This function creates a labeled frame widget, which is a container with a title label for grouping
+    other widgets within the GUI.
+
+    Args:
+        label (str): The text label to display on the labeled frame.
+        context (ImContext | None): The Immediate Mode GUI context in which the labeled frame should
+            be created. If not provided, the active context is used. (Default is None.)
+        **kwargs: Additional keyword arguments to configure the labeled frame widget.
+
+    Returns:
+        ImCursor: A cursor object for configuring and adding child widgets to the labeled frame.
+
+    Example:
+        # Create a labeled frame widget with the label "Settings"
+        with labelframe("Settings"):
+            # Within this block, all widget identifiers and actions are scoped to "Settings"
+            ...
+    """
     # Create the widget and add it
     context = context or base.get_context()
 
